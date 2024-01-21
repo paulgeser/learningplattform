5298b6186db3cf683decfad66f36e5fc48ae8d69
Paul Geser <paul.geser@roche.com>
2024-01-21 16:14:31 +0300

    implemented image analysis

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { createWorker } from 'tesseract.js';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes } from '@nestjs/swagger';
+
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post("/test")
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+
+  @UseInterceptors(FileInterceptor('file'))
+  async getTest(@UploadedFile() file: Express.Multer.File): Promise<string> {
+    console.log(file);
+    const worker = await createWorker('eng');
+    const ret = await worker.recognize(file.buffer, {}, { blocks: true });
+    console.log(ret.data.text);
+    await worker.terminate();
+    return Promise.resolve(ret.data.text);
+    return Promise.resolve("test");
   }
+
 }
