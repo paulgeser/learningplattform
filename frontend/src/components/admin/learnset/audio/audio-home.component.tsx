b77c8cc99e2982dc205cba2d0d0c4a1add53b8c3
Paul Geser <paul.geser@roche.com>
2024-01-29 22:19:15 +0300

    tried to implement audio splitting

--- frontend/src/components/admin/learnset/audio/audio-home.component.tsx
+++ frontend/src/components/admin/learnset/audio/audio-home.component.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getLearnSetById, getWordsByLearnSetId } from '../../../../services/learnset.service';
 import { LearnSet } from '../../../model/learnset.model';
 import { Word } from '../../../model/word.model';
+import WaveSurfer from 'wavesurfer.js';
+import { useWavesurfer } from '@wavesurfer/react';
 
 export const AudioHomeComponent: React.FC = (): React.ReactElement => {
     const [progressAndData, setProgressAndData] = useState<ProgressAndDataModel>({
@@ -35,6 +37,70 @@ export const AudioHomeComponent: React.FC = (): React.ReactElement => {
         }
     }
 
+    /* const handleFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log((event as any).target.result)
+        if (event.target.files && event.target.files.length > 0 && event.target) {
+            /* const reader = new FileReader();
+            reader.onload = function (event) {
+                const base64String = event.target.result.split(',')[1];
+                console.log(base64String);
+            };
+            reader.readAsDataURL(file);
+            var reader = new FileReader();
+            reader.onload = function (eventT) {
+                if (eventT.target && eventT.target.result && typeof eventT.target.result === 'string') {
+                    var data = eventT.target.result.split(',')
+                    var decodedImageData = btoa(data[1]);                    // the actual conversion of data from binary to base64 format
+                    console.log(decodedImageData, data[0], btoa(data[1]));
+                }
+            };
+            reader.readAsDataURL(event.target.files[0]);
+        } else {
+            alert('Please select an MP3 file.');
+        }
+    }; */
+
+
+
+    const [audioFile, setAudioFile] = useState<any>(null);
+
+    const handleFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (event.target.files) {
+            const file = event.target.files[0];
+            setAudioFile(file);
+        }
+    }
+
+    const waveformRef = useRef<any>();
+
+    let wavesurfer = null;
+
+    const initializeWaveform = () => {
+        console.log(waveformRef.current);
+        if (waveformRef.current) {
+            const wavesurfer = WaveSurfer.create({
+                container: waveformRef.current,
+            });
+            console.log(wavesurfer);
+        }
+
+        /* wavesurfer = WaveSurfer.create({
+            container: waveformRef.current,
+            waveColor: 'violet',
+            progressColor: 'purple',
+            cursorWidth: 1,
+            barWidth: 2,
+            height: 200
+        }); */
+
+
+/*         wavesurfer.load(audioFile);
+ */    };
+
+    const handleCut = () => {
+        console.log("test");
+    }
+
     return (
         <div style={{ display: 'flex', justifyContent: 'center' }} >
             <div style={{ maxWidth: '1400px', width: '100%', marginTop: '30px' }}>
@@ -46,7 +112,7 @@ export const AudioHomeComponent: React.FC = (): React.ReactElement => {
                             type="file"
                             name="fileInput"
                             accept="audio/*"
-                            onChange={(e) => onFileAdded(e.target.files)} />
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleFileInputChange(e)} />
                     </div>
                     {/* {progressAndData.progress === 0 && (
                         <PicturesFileUploadComponent progressAndData={progressAndData} setProgressAndData={setProgressAndData} />
@@ -54,6 +120,11 @@ export const AudioHomeComponent: React.FC = (): React.ReactElement => {
                     {progressAndData.progress === 1 && (
                         <PictureCropImagesComponent learnSetId={String(id)}  progressAndData={progressAndData} setProgressAndData={setProgressAndData} words={words} />
                     )} */}
+                    <div>
+                        <div ref={waveformRef} style={{ width: '100%', height: '200px' }} />
+                        <button onClick={initializeWaveform}>Load Audio</button>
+                        <button onClick={handleCut}>Cut Selected Portion</button>
+                    </div>
                 </React.Fragment>
             </div>
         </div>
